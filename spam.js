a88eaef8d5a1636956a1872457841529fe565bca
Tripl0Color <62907882+Tripl0Color@users.noreply.github.com>
2020-04-11 22:05:52 +0300

    Маленькие фиксы

--- spam.js
+++ spam.js
@@ -10,7 +10,7 @@ colors.setTheme({
   
 });
 const vk = new VK({
-token: '864e6064bc8193ed284bf78180b170ad8632ed32e5560779ce887cd18932272893e4944c1c0e239a179f6'
+token: 'PASTE TOKEN HERE'
 });
 console.log("By Tripl_color , awoken.xyz".info);
 
@@ -18,7 +18,7 @@ function startamuu() {
 vk.api.wall.createComment({
 owner_id: 472165736,
 post_id: 71626,
-message: 'Небесное свечение,сожги его до тла!'
+message: 'Небесное свечение,сожги его дотла!'
 });
 console.log("Коментарий сделан успешно".success);
 
