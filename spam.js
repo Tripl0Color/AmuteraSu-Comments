4a9c2b7ac73274f4db97e67e26f18944eb049b1e
Tripl0Color <62907882+Tripl0Color@users.noreply.github.com>
2020-04-12 15:50:56 +0300

    Подсказки в коде,улучшение читабельности кода

--- spam.js
+++ spam.js
@@ -1,29 +1,23 @@
-const { VK } = require('vk-io');
-var colors = require('colors');
-colors.setTheme({
-  info: 'bgGreen',
-  help: 'cyan',
-  warn: 'yellow',
-  success: 'rainbow',
-  error: 'red',
-  awok3n: 'italic',
-  
+const { VK } = require('vk-io'); /// Импортирование главной библиотеки
+var colors = require('colors');  /// Импортирование цвета консоли
+colors.setTheme({                 /// Импортирование цвета консоли
+  info: 'bgGreen',                 /// Импортирование цвета консоли
+  success: 'rainbow',               /// Импортирование цвета консоли
 });
 const vk = new VK({
-token: 'PASTE TOKEN HERE'
+token: 'PASTE TOKEN HERE'            /// Вставь токен сюда
 });
-console.log("By Tripl_color , awoken.xyz".info);
-
+console.log("By Tripl_color , awoken.xyz".info);      /// F Колору
 function startamuu() {
 vk.api.wall.createComment({
-owner_id: 472165736,
-post_id: 71626,
-message: 'Небесное свечение,сожги его дотла!'
+owner_id: your_vk_id,         /// Вставь свой айди сюда
+post_id: your_postid,        /// Вставь айди поста сюда
+message: 'vk.com/triplcolor @muuT3r@$$uu b2' /// Сюда сообщение которое будет в коментах
 });
-console.log("Коментарий сделан успешно".success);
-
+console.log("Коментарий сделан успешно".success);  /// Лог,о том что комент сделан
 }
 
-setInterval(startamuu, 1500);
+setInterval(startamuu, 1500);                /// Интервал меж коментами
 
                                                            
+/// Если будут какие-то вопросы, vk.com/tripl_color
