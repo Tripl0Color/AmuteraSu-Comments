8b11448f28dc96784a30c5d87d1e74fe0ba6c194
Tripl0Color <62907882+Tripl0Color@users.noreply.github.com>
2020-09-17 19:59:11 +0300

    Update spam.js

--- spam.js
+++ spam.js
@@ -5,19 +5,19 @@ colors.setTheme({                 /// Импортирование цвета к
   success: 'rainbow',               /// Импортирование цвета консоли
 });
 const vk = new VK({
-token: 'PASTE TOKEN HERE'            /// Вставь токен сюда
+token: 'вот сюда пихай токен'            /// Вставь токен сюда
 });
 console.log("By Tripl_color , awoken.xyz".info);      /// F Колору
 function startamuu() {
 vk.api.wall.createComment({
 owner_id: your_vk_id,         /// Вставь свой айди сюда
 post_id: your_postid,        /// Вставь айди поста сюда
-message: 'vk.com/triplcolor @muuT3r@$$uu b2' /// Сюда сообщение которое будет в коментах
+message: 'vk.com/tripl_color Amuterassuu-node' /// Сюда сообщение которое будет в коментах
 });
 console.log("Коментарий сделан успешно".success);  /// Лог,о том что комент сделан
 }
 
 setInterval(startamuu, 1500);                /// Интервал меж коментами
 
-                                                           
+/// Установи библиотеки : colors,vk-io                                                       
 /// Если будут какие-то вопросы, vk.com/tripl_color
